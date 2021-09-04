86ef0408ca64cf03396a72e0753c20563ad112a7
OneginForte <oneginforte@yahoo.com>
2021-09-04 19:47:29 +0500

    next

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "D:/Projects/ESP32-Z21/ESP32_Z21/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "D:/Users/oneg/esp/esp-idf/.git")
+set(GIT_DIR "D:/Projects/ESP32-Z21/ESP32_Z21/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
